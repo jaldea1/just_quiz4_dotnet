035106cac3e348bef2039ece81a9c31b3a18c5cd
AldeaJ <jaldea1@my.centennialcollege.ca>
2026-04-09 11:18:12 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using just_quiz4_dotnet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace just_quiz4_dotnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Database access has aleady been done.";
+            return View();
+        }
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace just_quiz4_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
